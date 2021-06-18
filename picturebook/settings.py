317ed6e003b101df9c1ef91eb792f35c5b1aaecc
Abimael Villarreal <avillarreal326@gmail.com>
2021-06-18 00:40:03 -0500

    pic solve try 3

--- picturebook/settings.py
+++ picturebook/settings.py
@@ -130,7 +130,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'pictureapp/static/images')
 
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'pb-picture-bucket'
+AWS_STORAGE_BUCKET_NAME = 'pb-picture-bucket/images'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
